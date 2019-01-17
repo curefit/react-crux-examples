e2b163fec78344d85079c0d5569fdedf9228892d
Jagadeesh YM <jagadeesh.ym@CF-LP00222.local>
2019-01-17 12:49:25 +0530

    Adding Example to Iterable

--- src/examples/Iterable.js
+++ src/examples/Iterable.js
@@ -36,6 +36,82 @@ const schema = {
 
 const Employees = CruxComponentCreator.create(schema)
 
+const reorderSchema = {
+    modelName: "employees",
+    title: "Employees with list of free-form Tags (REORDER Buttons)",
+    creationTitle: "Employee",
+    editModal: true,
+    fields: [
+        {
+            title: "Name",
+            field: "name",
+            editable: true,
+            representative: true,
+            display: true
+        },
+        {
+            title: "Age",
+            editable: true,
+            display: true,
+            field: "age"
+        },
+        {
+            title: "Tags",
+            editable: true,
+            display: true,
+            field: "tags",
+            type: "iterable",
+            additionalButtons: {
+                reorder: true
+            },
+            iterabletype: {
+                title: "Tag"
+            }
+        }
+    ],
+    createModal: true
+}
+
+const ReorderEmployees = CruxComponentCreator.create(reorderSchema)
+
+const addAtIndexSchema = {
+    modelName: "employees",
+    title: "Employees with list of free-form Tags (Add At Index Button)",
+    creationTitle: "Employee",
+    editModal: true,
+    fields: [
+        {
+            title: "Name",
+            field: "name",
+            editable: true,
+            representative: true,
+            display: true
+        },
+        {
+            title: "Age",
+            editable: true,
+            display: true,
+            field: "age"
+        },
+        {
+            title: "Tags",
+            editable: true,
+            display: true,
+            field: "tags",
+            type: "iterable",
+            additionalButtons: {
+                addAtIndex: true
+            },
+            iterabletype: {
+                title: "Tag"
+            }
+        }
+    ],
+    createModal: true
+}
+
+const EmployeesAddAtIndex = CruxComponentCreator.create(addAtIndexSchema)
+
 const customButtonIterableSchema = {
     modelName: "employees",
     title: "Employees with list of free-form Tags with custom button",
@@ -61,8 +137,10 @@ const customButtonIterableSchema = {
             display: true,
             field: "tags",
             type: "iterable",
-            customIterableButton: true,
-            customButtonAction: customButtonAction,
+            additionalButtons: {
+                customButton: true,
+                customButtonAction: customButtonAction,
+            },
             iterabletype: {
                 title: "Tag"
             }
@@ -83,6 +161,14 @@ class Iterable extends Component {
             <div style={{ display: "flex", padding: 20, borderBottom: "1px solid #EEE" }}>
                 <div style={{ width: 500 }}><Employees /></div>
                 <pre>{JSON.stringify(schema, null, 2)}</pre>
+            </div>
+            <div style={{ display: "flex", padding: 20, borderBottom: "1px solid #EEE" }}>
+                <div style={{ width: 500 }}><ReorderEmployees /></div>
+                <pre>{JSON.stringify(reorderSchema, null, 2)}</pre>
+            </div>
+            <div style={{ display: "flex", padding: 20, borderBottom: "1px solid #EEE" }}>
+                <div style={{ width: 500 }}><EmployeesAddAtIndex /></div>
+                <pre>{JSON.stringify(addAtIndexSchema, null, 2)}</pre>
             </div>
             <div style={{ display: "flex", padding: 20, borderBottom: "1px solid #EEE" }}>
                 <div style={{ width: 500 }}><CustomButtonIterable /></div>
